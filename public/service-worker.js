4410429824ea684359b00ad3f5c2ce34f572ca8b
Daniel Sasser <desasser79@gmail.com>
2021-02-10 13:31:59 -0800

    debugging

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,9 +5,8 @@ const FILES_TO_CACHE = [
   "/styles.css",
   "/db.js",
   "/manifest.webmanifest",
-  "/favicon.ico",
-  "/assets/images/icons/icon-192x192.png",
-  "/assets/images/icons/icon-512x512.png",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
 ];
 
 const CACHE_NAME = "static-cache-v2";
